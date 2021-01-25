fed9f767d90e8abcf7b694350d2bb7c3a98b98f2
WCharonski <77988571+WCharonski@users.noreply.github.com>
2021-01-25 21:01:53 +0100

    Add files via upload

---
+++ Core/Inc/States.h
@@ -0,0 +1,14 @@
+/*
+ * States.h
+ *
+ *  Created on: Jan 23, 2021
+ *      Author: Wojtek
+ */
+
+#ifndef INC_STATES_H_
+#define INC_STATES_H_
+
+#endif /* INC_STATES_H_ */
+
+void States_Change (double TempRef, double temp);
+void States_Run();
